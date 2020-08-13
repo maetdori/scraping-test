fa0e2dfa4e3e7c81084dc2da1eb43c2f364bb928
이해인 <odong2@flyhigh-x.com>
2020-08-13 18:03:06 +0900

    payinfo 새로 구현 !

--- payinfo.py
+++ payinfo.py
@@ -1,18 +1,19 @@
+import requests
 from bs4 import BeautifulSoup
-import urllib.request
-import datetime
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import re
-
-import send_email
-
+import http.cookiejar
+import urllib
 import check_mail
-import time
+import send_email
+import datetime
 import json
 import logging
+from email.mime.image import MIMEImage
+import PIL
+import pytesseract
+from PIL import Image
+from pytesseract import *
+import re
+import cv2
 
 #log config
 with open("./globalval.json",'r') as file:
@@ -24,66 +25,53 @@ chromedriver = json_data["chromedrive_exe_path"]
 logging.basicConfig(filename=file_name,level=log_level)
 
 def go():
-    # ===================================================
-    # 메일에 들어갈 내용
+    #===================================================#
+    #메일에 들어갈 내용
     mail_body = {}
-    # 오늘날짜
+    abort_date = "N"
+    abort_thing = "N"
+    abort_why = "N"
+
+    #오늘날짜
     today = datetime.datetime.now()
-    logging.debug("payinfo : "+str(today))
-    # ===================================================
-    # 헤드리스
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument("disable-gpu")
-    driver = webdriver.Chrome(chromedriver, options=options)
-    # 사이트 접속
-    wait = WebDriverWait(driver, 20)
-    driver.get('https://www.payinfo.or.kr/index.do')
-    time.sleep(5)
-    driver.switch_to.frame('startframe')
-    driver.switch_to.frame('startframe')
-    driver.find_element_by_xpath('//*[@id="gnb"]/li[7]/a').click()
+    #===================================================#
 
-    # 데이터 수집
-    for i in range(1,4):
-        xpath = '//*[@id="contents"]/div/table/tbody/tr[%d]/td[2]'%i
-        if i > 1:
-            driver.switch_to.frame('startframe')
-            driver.switch_to.frame('startframe')
-        wait.until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
-        time.sleep(2)
-        html = driver.page_source
-        soup = BeautifulSoup(html,'html.parser')
-        
-        try:
-            # 날짜 구하기
-            notice_date = str(soup.find_all('span', {'class': 'bar'}))
-            notice_date = re.sub('<.+?>', '', notice_date, 0).strip().replace('[', '').replace(']', '').split('.')
-            notice_date = datetime.date(int(notice_date[0]),int(notice_date[1]),int(notice_date[2]))
-        except:
-            return "!!!계좌정보 통합관리 에러!!!"
-        title = str(soup.find('th',{'scope':'col'}))
-        title = re.sub('<.+?>', '', title, 0).strip().replace('[','').replace(']','')
-        # 오늘 날짜와 같다면?
-        if today == notice_date:
-            # 이미지
-            soup = soup.find('td', {'class': 'context'})
-            if 'img' in str(soup):
-                imgUrl = soup.find('img').get('src')
-                mail_body[title] = imgUrl
-                urllib.request.urlretrieve(imgUrl, "." + 'adldi%d'%i)
-            # 텍스트
-            else:
-                text = str(soup).replace('<br/>','\n')
-                text = re.sub('<.+?>', '', text, 0).strip().replace('[', '').replace(']', '')
-                mail_body[title] = text
-        else:
-            break
-        driver.back()
+    #===================================================#
+    # 공지사항 화면 체크 
 
-    driver.quit()
-    mail_body = dict(sorted(mail_body.items(), key=lambda x: x[0]))
-    return check_mail.check('계좌정보 통합관리', mail_body)
+    # make session
+    cj = http.cookiejar.LWPCookieJar()
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    url = "https://www.payinfo.or.kr/main/main.do?mode=null"
+    html = opener.open(url).read().decode("utf-8")
 
+    soup = BeautifulSoup(html, 'html.parser')
+    noti = soup.find('span', {'class': 'notice_conlist'})
+    title = noti.get_text().split("(")[0]
 
+    #if "점검" in title or "중단" in title:
+    href = noti.find('a').attrs['href']
+
+    # 상세페이지
+    url = "https://www.payinfo.or.kr" + href
+    html = opener.open(url).read().decode("utf-8")
+
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find('table', {'class': 'tbl_view'})
+
+    # 공지사항 이미지 받아오기
+    img_url = table.find('img').attrs['src']
+    urllib.request.urlretrieve(img_url, 'payinfo' + '.png')
+
+    # 이미지 텍스트 변환
+    img = Image.open('payinfo.png')
+    text = pytesseract.image_to_string(img, lang='kor')
+
+    content = text.replace("\nㅁ", "-").split("\n")
+    for element in content:
+        if "점검" in element and str(today.month) + "." + str(today.date) in element:
+        #if "2019.12.30" in element:
+            abort_date = element
+            mail_body[title] = abort_date
+
+    return check_mail.check("계좌정보통합관리서비스", mail_body)
